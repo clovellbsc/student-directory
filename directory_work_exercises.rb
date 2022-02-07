08b204a93451ec88f2a6d4a9ef898a2e2be50a01
Chris <clovellbsc@gmail.com>
2022-02-07 16:08:48 +0000

    Printing only students with names less than 12 characters long

--- directory_work_exercises.rb
+++ directory_work_exercises.rb
@@ -21,7 +21,7 @@ end
 
 def print(students)
   students.each_with_index do |student, index|
-    puts "#{index + 1}. #{student[:name]} (#{student[:cohort]} cohort)" if student[:name].upcase.start_with?("S") == true
+    puts "#{index + 1}. #{student[:name]} (#{student[:cohort]} cohort)" if student[:name].length  < 12
   end
 end
 
